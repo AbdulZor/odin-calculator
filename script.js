9f6a156eb8dc07f7fc9b9f61f489f51da32ab8a5
AbdulZor <abdulzor@outlook.com>
2024-09-22 10:25:41 +0200

    Add arithmetic to UI
    Arithmetic operations are presented in the UI.
    Changed the operate() function: in the add operation, the arguments are
    first converted to number because display value is a string, which would
    concat two strings instead of numbers.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let rightOperand;
 function operate(operator, operandX, operandY) {
     switch (operator) {
         case '+':
-            return add(operandX, operandY);
+            return add(+operandX, +operandY);
         case '-':
             return subtract(operandX, operandY);
         case '*':
@@ -30,3 +30,63 @@ function operate(operator, operandX, operandY) {
             return divide(operandX, operandY);
     }
 }
+
+
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+let displayVal;
+numbers.forEach((val) => {
+    val.addEventListener('click', () => {
+        if (display.textContent === operator) {
+            display.textContent = '';
+        }
+        display.textContent += val.textContent;
+        console.log(`display.textContent = ${display.textContent}`);
+    });
+});
+
+
+
+// When numbers are entered and then an operation is clicked, we need to store the left number as the left operand.
+// However, if there is already a left operand value, we need to apply the last operator function to it and then
+// store the new result as the left operand.
+// PSEUDO
+// AT NEW OPERATOR ENTER:
+// IF left_operand NOT NULL AND operator NOT NULL:
+//      left_operand = operate(operator, left_operand, display_value)
+//      operator = clicked_value
+//      display_value = clicked_value
+
+const operators = document.querySelectorAll('.operation');
+operators.forEach((val) => {
+    val.addEventListener('click', () => {
+        switch (val.textContent) {
+            case '=':
+                if (leftOperand != null && operator != null) {
+                    console.log(display.textContent)
+                    rightOperand = display.textContent;
+                    display.textContent = operate(operator, leftOperand, rightOperand);
+                    leftOperand = display.textContent;
+                    operator = null;
+                    rightOperand = null;
+                }
+                break;
+            case 'C':
+                leftOperand = null;
+                rightOperand = null;
+                operator = null;
+                display.textContent = '';
+                break;
+            default: // normal arithmetic operations
+                if (leftOperand != null && operator != null) {
+                    leftOperand = operate(operator, leftOperand, displayVal);
+                } else {
+                    leftOperand = display.textContent;
+                }
+                operator = val.textContent;
+                display.textContent = val.textContent;
+                break;
+        }
+
+    })
+});
