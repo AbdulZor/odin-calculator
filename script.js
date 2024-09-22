72e6d8f991ca5d155de5dfacfd037258866df6f1
AbdulZor <abdulzor@outlook.com>
2024-09-22 07:24:39 +0200

    Add function that evaluates basic math  expression

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+let leftOperand;
+let operator;
+let rightOperand;
+
+function operate(operator, operandX, operandY) {
+    switch (operator) {
+        case '+':
+            return add(operandX, operandY);
+        case '-':
+            return subtract(operandX, operandY);
+        case '*':
+            return multiply(operandX, operandY);
+        case '/':
+            return divide(operandX, operandY);
+    }
+}
